0c2701570a838ae6e956cd5c091009487e181fb5
bdavidson13 <brian.j.davidson13@gmail.com>
2017-12-20 08:15:43 -0500

    Finished view model for user

---
+++ vincent/Models/Skill.cs
@@ -0,0 +1,5 @@
+public class Skill
+{
+    public string Name { get; set; }
+    public int Id { get; set ; }
+}
--- vincent/Models/UserViewModel.cs
+++ vincent/Models/UserViewModel.cs
@@ -1,6 +1,10 @@
-
+using System.Collections.Generic;
 public class UserViewModel
 {
     public string FirstName { get; set; }
     public string LastName { get; set; } 
+    public string Email { get; set; }
+    public string Password { get; set; }
+    public string PhoneNumber { get; set; }
+    public List<Skill> Skills { get; set; }
 }
