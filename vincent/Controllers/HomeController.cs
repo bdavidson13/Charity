503266e80abb4247ff18d1c19021684a0dd9ab20
Brian Davidson <brian.j.davidson13@gmail.com>
2018-02-14 09:03:38 -0500

    Got login path working (get and post)

--- vincent/Controllers/HomeController.cs
+++ vincent/Controllers/HomeController.cs
@@ -20,6 +20,25 @@ namespace vincent.Controllers
             return View();
         }
 
+        public IActionResult Login(){
+            UserViewModel model = new UserViewModel();
+            return View(model);
+        }
+        [HttpPost]
+        public IActionResult Login(UserViewModel model){
+            if(!ModelState.IsValid){
+                return View(model);
+            }
+            /*TODO: 
+                1) get User back from service
+                2) if user doesn't exist return error
+                3) if uses does exist save user data in session var
+                4) redirect home
+                5) once that's working go back and save where uses is
+                   coming from and redirect back to there
+            */
+            return View("Index");
+        }
         public IActionResult About()
         {
             ViewData["Message"] = "Your application description page.";
